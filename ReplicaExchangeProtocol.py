1d4d3d69944cd0939bb95753dec33259fa4c304d
jacopoventurin <82530776+jacopoventurin@users.noreply.github.com>
2022-12-10 13:13:42 +0100

    last update for replica exchange

--- ReplicaExchangeProtocol.py
+++ ReplicaExchangeProtocol.py
@@ -1,10 +1,7 @@
 import numpy as np
-import random
-#from six.moves import cPickle
-#import _pickle as cPickle
-import pickle
 from openmmtools import cache
 import openmm.unit as unit
+import openmm as mm
 
 class ReplicaExchange:
 
@@ -14,134 +11,53 @@ class ReplicaExchange:
         sampler_states=None,
         mcmc_move=None,
     ):
-        try:
-            self._thermodynamic_states = thermodynamic_states
-            self._replicas_sampler_states = sampler_states
-            self._mcmc_move = mcmc_move
-            self.n_replicas = len(thermodynamic_states)
-            self._temperature_list = [self._thermodynamic_states[i].temperature for i in range(self.n_replicas)]
-        except:
-            pass
+        self._thermodynamic_states = thermodynamic_states
+        self._replicas_sampler_states = sampler_states
+        self._mcmc_move = mcmc_move
+        self.n_replicas = len(thermodynamic_states)
+        self._temperature_list = [self._thermodynamic_states[i].temperature for i in range(self.n_replicas)]
 
-    def run(self, 
-            n_iterations=1, 
-            save=False, 
-            save_interval:int = 1, 
-            forces_path:str = None, 
-            coords_path:str = None,
-            mode:str = 'all'):
-
-        """
-        Perform integration of the all system, and attempt to exchange 
-        replicas.
-
-        Params
-        ------
-        n_iteration:
-            total number of timesteps for the evolution of the system
-        save:
-            if set to True position, forces and acceptance matrix are saved, default is False
-        save_interval:
-            length of position and forces array saved, default is 100.
-            Must be a multiple of n_interaction
-        coords_path:
-            path where to save forces
-        forces_path: 
-            path where to save coords
-        mode: 
-            can be 'all', then exchange between any possible couple 
-            of replicas is attempted, or 'neighbors', then exchange 
-            between replicaswith neighbors temperatures only 
-            is attempted
-        Return
-        ------
-            acceptance matrix
-        """
-
-        if n_iterations % save_interval != 0:
-            raise ValueError(f"save_interval must be intere multipole of n_iterations, you set {save_interval} and {n_iterations}")
-
-        if forces_path is None:
-            forces_path = 'forces_'
-        else:
-            forces_path = forces_path + '/forces'
-        if coords_path is None:
-            coords_path = 'coords_'
-        else:
-            coords_path = coords_path + '/coords'
-
-        positions = []
-        forces = []
+    def run(self, n_iterations=1, save=False, save_interval:int = 1, checkpoint_simulations=False):
+        self.positions = []
+        self.forces = []
         self.acceptance_matrix = np.zeros((self.n_replicas, self.n_replicas))
-        count = 0
-
-        print('Start symulation')
-        for iteration in range(n_iterations):               
+        for iteration in range(n_iterations):
+            if iteration % 100 == 0: print(iteration)
             self._propagate_replicas()
-            self._mix_replicas(mode = mode)
+            self._mix_replicas()
 
             # Save positions and forces
             if save:
-                positions.append([])
-                forces.append([])
-                pos, forc = self._grab_forces()
-                positions[-1] = pos
-                forces[-1] = forc
+                if iteration % save_interval == 0:
+                    self.positions.append([])
+                    self.forces.append([])
+                    positions, forces = self._grab_forces()
+                    self.positions[-1] = positions
+                    self.forces[-1] = forces
 
-            if (iteration+1) % save_interval == 0: 
-                print(f'Interaction {iteration} of {n_iterations}')
-                if save:    
-                    np.save(f'{coords_path}_{count}.npy',np.swapaxes(np.array(positions),0,1))
-                    np.save(f'{forces_path}_{count}.npy',np.swapaxes(np.array(forces),0,1))
-                    # Empty memory
-                    positions = []
-                    forces = []
-                    count += 1
-        
-        return self.acceptance_matrix
+            if checkpoint_simulations:
+                self._save_contexts()
 
-    def save(self, name):
-        """save class as name.txt"""
-        file = open(name+'.txt','wb')
-        pickle.dump(self.__dict__, file)
-        file.close()
-
-    def load(self, name):
-        """try load name.txt"""
-        file = open(name+'.txt','rb')
-        self.__dict__ = pickle.load(file)
-        file.close()
-
+        if save:
+            ## Output in the format shape
+            # replica, frames, n_atoms, xyz
+            positions = np.swapaxes(np.array(self.positions),0,1)
+            forces = np.swapaxes(np.array(self.forces),0,1)
+            return positions,forces,self.acceptance_matrix
+        else:
+            return _,_,self.acceptance_matrix
 
     def _propagate_replicas(self):
         # _thermodynamic_state[i] is associated to the replica configuration in _replicas_sampler_states[i].
         for thermo_state, sampler_state in zip(self._thermodynamic_states, self._replicas_sampler_states):
             self._mcmc_move.apply(thermo_state, sampler_state)
 
-    def _mix_replicas(self, n_attempts:int =1, mode:str = 'all'):
-        """
-        Attempt to switch two replicas at random or a couple of 
-        adiacent temperature replicas. 
-        Obviously, this scheme can be improved.
-        Params:
-        ------
-        n_attempts:
-            number of attempts to exchange a couple of replicas
-        mode:
-            can be 'all', then exchange between any possible couple 
-            of replicas is attempted, or 'neighbors', then exchange 
-            between replicaswith neighbors temperatures only 
-            is attempted
-        """
-
+    def _mix_replicas(self, n_attempts=1):
+        # Attempt to switch two replicas at random. Obviously, this scheme can be improved.
         random_number_list = np.random.rand(n_attempts)
-
         for attempt in range(n_attempts):
             # Select two replicas at random.
-            if mode == 'all':
-                i,j = np.sort(np.random.choice(range(len(self._thermodynamic_states)),2,replace=False))
-            elif mode == 'neighbors':
-                i,j = self._adiacent()
+            i,j = np.sort(np.random.choice(range(len(self._thermodynamic_states)),2,replace=False))
             self.acceptance_matrix[j,i] += 1
             sampler_state_i, sampler_state_j = (self._replicas_sampler_states[k] for k in [i, j])
             thermo_state_i, thermo_state_j = (self._thermodynamic_states[k] for k in [i, j])
@@ -159,24 +75,6 @@ class ReplicaExchange:
                 self._thermodynamic_states[i] = thermo_state_j
                 self._thermodynamic_states[j] = thermo_state_i
                 self.acceptance_matrix[i,j] += 1
-
-    def _adiacent(self):
-        """
-        Find random indeces of adiacent replicas
-        """
-        temps = len(self._thermodynamic_states)-1
-        i = np.random.randint(0, temps)
-        j = i + 1
-        k = i - 1
-
-        if j  > temps:
-            return i, k
-        elif k < 0:
-            return i, j
-        else:
-            jk = random.choice([j,k])
-            return i, jk
-
     def _compute_reduced_potential(self, sampler_state, thermo_state):
         # Obtain a Context to compute the energy with OpenMM. Any integrator will do.
         context, _ = cache.global_context_cache.get_context(thermo_state)
@@ -201,4 +99,20 @@ class ReplicaExchange:
             forces[i_t] = force
         return positions, forces
 
-            
+    def _save_contexts(self):
+        for i_t,(thermo_state, sampler_state) in enumerate(zip(self._thermodynamic_states, self._replicas_sampler_states)):
+            context, _ = cache.global_context_cache.get_context(thermo_state)
+            sampler_state.apply_to_context(context)
+            state = context.getState(getPositions=True, getVelocities=True, getParameters=True)
+            state_xml = mm.XmlSerializer.serialize(state)
+            with open('checkpoint-{}.xml'.format(i_t), 'w') as output:
+                output.write(state_xml)
+
+    def _load_contexts(self):
+        for i_t,(thermo_state, sampler_state) in enumerate(zip(self._thermodynamic_states, self._replicas_sampler_states)):
+            context, _ = cache.global_context_cache.get_context(thermo_state)
+            with open('checkpoint-{}.xml'.format(i_t), 'r') as input:
+                state = mm.XmlSerializer.deserialize(input.read())
+                sampler_state.positions = state.getPositions()
+                sampler_state.velocities = state.getVelocities()
+            sampler_state.apply_to_context(context)

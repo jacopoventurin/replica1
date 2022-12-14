1afe7db940b98be2b331719e58e241cccb56fb46
jacopoventurin <venturinjacopo@gmail.com>
2022-12-14 09:28:28 +0100

    prepare for main branch

--- ReplicaExchangeProtocol.py
+++ ReplicaExchangeProtocol.py
@@ -17,23 +17,56 @@ class ReplicaExchange:
         self.n_replicas = len(thermodynamic_states)
         self._temperature_list = [self._thermodynamic_states[i].temperature for i in range(self.n_replicas)]
 
-    def run(self, n_iterations=1, save=False, save_interval:int = 1, checkpoint_simulations=False):
+    def run(self, 
+            n_iterations:int = 1, 
+            n_attempts:int = 1, 
+            equilibration_timesteps:int =1,
+            production_timesteps:int =1,
+            save:bool = False, 
+            save_interval:int = 1, 
+            checkpoint_simulations:bool = False, 
+            mixing:str = 'all'):
+        """
+        Run a simulation with replica exchange protocol. Is possible to try exchange between 
+        all replicas or just between neighbors.
+
+        Params
+        ------
+        n_iteration:
+            number of iteration to perform during the all symulation, default is 1.
+        n_attempts:
+            number of attempts to exchange a pair of replicas for each iteration, default is 1.
+        equilibraton_timesteps:
+            how many times apply mcm_move for the equilibration phase.
+        production_timesteps:
+            how many times apply mcm_move for the production phase. 
+        save:
+            if set to True position and forces are saved every save_interval steps during 
+            the production phase. Position and forces are returned as numpy array.
+        save_interval:
+            interval for save position and forces, default is 1.
+        checkpoint_simulations:
+            if set to True a checkpoint configuration is saved every iteration.
+        mixing:
+            can be 'all' or 'neighbors'. If 'all' exchange between all possible replicas is attempted,
+            while if 'neighbors' exchange between neighbors replicas only is attempted. If 'all' then 
+            n_attempts should be at least n_replicas*log(n_replicas), where n_replicas is the total 
+            number of replicas in the simulation. See https://aip.scitation.org/doi/10.1063/1.3660669
+            for more information.
+        """
         self.positions = []
         self.forces = []
         self.acceptance_matrix = np.zeros((self.n_replicas, self.n_replicas))
+        if mixing == 'neighbors':
+            self._define_neighbors()
         for iteration in range(n_iterations):
             if iteration % 100 == 0: print(iteration)
-            self._propagate_replicas()
-            self._mix_replicas()
-
-            # Save positions and forces
-            if save:
-                if iteration % save_interval == 0:
-                    self.positions.append([])
-                    self.forces.append([])
-                    positions, forces = self._grab_forces()
-                    self.positions[-1] = positions
-                    self.forces[-1] = forces
+            # Eequilibration
+            self._propagate_replicas(n_timesteps=equilibration_timesteps, save=False)
+            # Production
+            self._propagate_replicas(n_timesteps=production_timesteps, save=True, save_interval=1)
+            # Exchange between replicas
+            self._mix_replicas(mixing=mixing, n_attempts=n_attempts)
 
             if checkpoint_simulations:
                 self._save_contexts()
@@ -45,19 +78,51 @@ class ReplicaExchange:
             forces = np.swapaxes(np.array(self.forces),0,1)
             return positions,forces,self.acceptance_matrix
         else:
-            return _,_,self.acceptance_matrix
+            return self.acceptance_matrix
 
-    def _propagate_replicas(self):
-        # _thermodynamic_state[i] is associated to the replica configuration in _replicas_sampler_states[i].
-        for thermo_state, sampler_state in zip(self._thermodynamic_states, self._replicas_sampler_states):
-            self._mcmc_move.apply(thermo_state, sampler_state)
+    def _propagate_replicas(self, n_timesteps:int = 1, save:bool = False, save_interval:int = 1):
+        """
+        Apply _mcm_move to all the replicas n_timesteps times. If save is set to True
+        position and forces are saved every save_interval time.
+        _thermodynamic_state[i] is associated to the replica configuration in _replicas_sampler_states[i].
+        """
+        for timestep in range(n_timesteps):
+            for thermo_state, sampler_state in zip(self._thermodynamic_states, self._replicas_sampler_states):
+                self._mcmc_move.apply(thermo_state, sampler_state)
+            if save:
+                if timestep % save_interval == 0:
+                    self.positions.append([])
+                    self.forces.append([])
+                    positions, forces = self._grab_forces()
+                    self.positions[-1] = positions
+                    self.forces[-1] = forces
 
-    def _mix_replicas(self, n_attempts=1):
-        # Attempt to switch two replicas at random. Obviously, this scheme can be improved.
+    def _mix_replicas(self, mixing:str = 'all', n_attempts=1,):
+        """
+        Mix replicas using two possible strategy: try to exchange two replicas randomly choosen 
+        between all possible states, or try to exchange a randomly choosen couple of neighbors of
+        neighbor states only.
+        
+        Params:
+        ------  
+        mixing:
+            can be 'all' or 'neighbors'. If 'all' exchange between all possible replicas is attempted,
+            while if 'neighbors' exchange between neighbors replicas only is attempted. If 'all' then 
+            n_attempts should be at least n_replicas*log(n_replicas), where n_replicas is the total 
+            number of replicas in the simulation. See https://aip.scitation.org/doi/10.1063/1.3660669
+            for more information.
+        n_attempts:
+            number of attempts to exchange a pair of replicas for each iteration, default is 1.
+        """
         random_number_list = np.random.rand(n_attempts)
         for attempt in range(n_attempts):
-            # Select two replicas at random.
-            i,j = np.sort(np.random.choice(range(len(self._thermodynamic_states)),2,replace=False))
+            if mixing == 'neighbors':
+                #select two random neighbors
+                i,j = np.sort(self.couples[np.random.randint(len(self.couples))])
+            elif mixing == 'all':
+                # Select two replicas at random
+                i,j = np.sort(np.random.choice(range(len(self._thermodynamic_states)),2,replace=False))
+            
             self.acceptance_matrix[j,i] += 1
             sampler_state_i, sampler_state_j = (self._replicas_sampler_states[k] for k in [i, j])
             thermo_state_i, thermo_state_j = (self._thermodynamic_states[k] for k in [i, j])
@@ -75,6 +140,19 @@ class ReplicaExchange:
                 self._thermodynamic_states[i] = thermo_state_j
                 self._thermodynamic_states[j] = thermo_state_i
                 self.acceptance_matrix[i,j] += 1
+
+
+    def _define_neighbors(self):
+        # Define all possible couples of neighbors
+        l = np.arange(len(self._thermodynamic_states))
+        couples = []
+        for i in zip(l, l[1:]):
+            couples.append(i)
+
+        self.couples = np.array(couples)
+
+
+
     def _compute_reduced_potential(self, sampler_state, thermo_state):
         # Obtain a Context to compute the energy with OpenMM. Any integrator will do.
         context, _ = cache.global_context_cache.get_context(thermo_state)

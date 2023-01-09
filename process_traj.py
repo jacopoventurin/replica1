0c5d46f3a9739f3d794068e3cea3be3fe0687267
jacopoventurin <venturinjacopo@gmail.com>
2023-01-09 16:29:38 +0100

    working on trajectories

--- process_traj.py
+++ process_traj.py
@@ -6,7 +6,7 @@ from glob import glob
 
 
 ## number of previous simulation saved
-delta = 0  
+delta = 20  
 
 ## path of the results from all thoms simulation 
 
@@ -14,8 +14,8 @@ delta = 0
 #pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim1'
 #forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim1'
 ## Exchange neighbors
-pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim1'
-forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim1'
+pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim3'
+forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim3'
 
 ## pdb file used for the simulation
 pdb_file = 'chi_sys.pdb'
@@ -69,7 +69,7 @@ for pos_fn, force_fn in zip(pos_fns, forces_fns):
 ##########################################################################################################################################################
 
 ## number of previous simulation saved
-delta = 10  
+delta = 30  
 
 ## path of the results from all thoms simulation 
 
@@ -77,8 +77,8 @@ delta = 10
 #pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim1'
 #forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim1'
 ## Exchange neighbors
-pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim2'
-forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim2'
+pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim4'
+forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim4'
 
 ## pdb file used for the simulation
 pdb_file = 'chi_sys.pdb'
@@ -130,13 +130,13 @@ for pos_fn, force_fn in zip(pos_fns, forces_fns):
 ########################################################################################################################################################################################################################
 
 ## number of previous simulation saved
-delta = 0  
+delta = 30  
 
 ## path of the results from all thoms simulation 
 
 ## Exchange all
-pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim1'
-forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim1'
+pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim4'
+forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim4'
 ## Exchange neighbors
 #pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim2'
 #forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim2'
@@ -195,13 +195,13 @@ for pos_fn, force_fn in zip(pos_fns, forces_fns):
 
 
 ## number of previous simulation saved
-delta = 10  
+delta = 40  
 
 ## path of the results from all thoms simulation 
 
 ## Exchange all
-pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim2'
-forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim2'
+pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim5'
+forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim5'
 ## Exchange neighbors
 #pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim2'
 #forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim2'
@@ -253,62 +253,3 @@ for pos_fn, force_fn in zip(pos_fns, forces_fns):
     print(f'Progress -> {i+1} over 10')
     i += 1
 ##################################################################################################################################################################################################
-
-
-## number of previous simulation saved
-delta = 20  
-
-## path of the results from all thoms simulation 
-
-## Exchange all
-pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim3'
-forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_all/sim3'
-## Exchange neighbors
-#pos_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim2'
-#forces_path = '/import/a12/users/jacopo/transf_temp/all_atom_replica_results_chignolin/exchange_neighbors/sim2'
-
-## pdb file used for the simulation
-pdb_file = 'chi_sys.pdb'
-
-
-# simulation parameters
-n_replicas = 36
-min_T = 280
-max_T = 400
-Temperatures = np.geomspace(min_T, max_T, n_replicas)
-
-## paths where to save results of trajectories elaboration are created
-
-## Excahge all 
-processed_pos = '/local_scratch2/jacopo/trans_temp/all_atom_replica_results_chignolin/exchange_all/coords_nowater'
-processed_forces = '/local_scratch2/jacopo/trans_temp/all_atom_replica_results_chignolin/exchange_all/forces_nowater'
-# Exchange neighbors
-#processed_pos = '/local_scratch2/jacopo/trans_temp/all_atom_replica_results_chignolin/exchange_neighbors/coords_nowater'
-#processed_forces = '/local_scratch2/jacopo/trans_temp/all_atom_replica_results_chignolin/exchange_neighbors/forces_nowater'
-
-
-# find protein's atoms indeces
-topo = md.load(pdb_file).topology
-protein_index = topo.select("protein")
-protein_topo = topo.subset(protein_index)
-
-
-
-
-# load position and forces
-pos_fns = sorted(glob(f"{pos_path}/position_*.npy"))
-forces_fns = sorted(glob(f"{forces_path}/forces_*.npy"))
-
-# verify position and forces have same length
-assert len(pos_fns) == len(forces_fns)
-
-i = 0
-print('Started')
-for pos_fn, force_fn in zip(pos_fns, forces_fns):    
-    pos = np.load(pos_fn)[:, :, protein_index, :]
-    force = np.load(force_fn)[:, :, protein_index, :]
-    for idx in tqdm(range(n_replicas)):
-        np.save(f"{processed_pos}/temperature_{Temperatures[idx]:.0f}/coor_{i+delta}", pos[idx]) 
-        np.save(f"{processed_forces}/temperature_{Temperatures[idx]:.0f}/forces_{i+delta}", force[idx]) 
-    print(f'Progress -> {i+1} over 10')
-    i += 1
